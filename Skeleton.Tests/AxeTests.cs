5bfce0d536970014caf9de3a2509a051c4603688
TrifShterev <tshterev@yahoo.com>
2020-11-20 21:23:39 +0200

    Add project files.

---
+++ Skeleton.Tests/AxeTests.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using System;
+[TestFixture]
+public class AxeTests
+{//По този начин може да създаваме различни test cases
+    [Test]
+    [TestCase(100, 100, 100, 100, 99)]
+    public void Weapon_Should_Lose_Durability_After_Attack(
+        int health,
+        int exp, 
+        int attack, 
+        int durability, 
+        int expectedResult)
+    {
+        //Arrange
+        
+        Dummy dummy = new Dummy(health, exp);
+        Axe axe = new Axe(attack, durability);
+
+        //Act
+        axe.Attack(dummy);
+
+        //Assert
+        
+        var actualResult = axe.DurabilityPoints;
+
+        Assert.AreEqual(expectedResult,actualResult);
+    }
+    [Test]
+    public void AttackShouldThrowInvalidOperetaionExceptionWhenAxeDurabilityIsBelowZero()
+    {
+        //Arrange
+        Dummy dummy = new Dummy(10, 10);
+        Axe axe = new Axe(20,0);
+
+        //Act-Assert
+        Assert.Throws<InvalidOperationException>(() => axe.Attack(dummy));
+
+        
+    }
+}
---
+++ Skeleton.Tests/DummyTests.cs
@@ -0,0 +1,60 @@
+﻿using NUnit.Framework;
+using System;
+
+[TestFixture]
+public class DummyTests
+{
+    [Test]
+    public void DummyShouldLoseHealthIfAttacked()
+    {
+        //Arrange
+        Dummy dummy = new Dummy(100,100);
+
+        //Act
+        dummy.TakeAttack(10);
+
+        //Assert
+        var expectedResult = 90;
+        var actualResult = dummy.Health;
+
+        Assert.AreEqual(expectedResult,actualResult);
+    }
+    [Test]
+    public void DeadDummyShouldThrowExceptionIfAttacked()
+    {
+        //Arrange
+        Dummy dummy = new Dummy(0, 100);
+
+
+
+        //Assert-Act
+
+
+        Assert.Throws<InvalidOperationException>(() => dummy.TakeAttack(10));
+    }
+    [Test]
+    public void DeadDummyShouldGiveXP()
+    {
+        //Arrange
+        Dummy dummy = new Dummy(0, 100);
+
+
+
+        //Assert-Act
+        var expectedResult = 100;
+        var actualResult = dummy.GiveExperience();
+
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+    [Test]
+    public void AliveDummyShouldNotGiveXP()
+    {
+        //Arrange
+        Dummy dummy = new Dummy(100, 100);
+
+
+
+        //Assert-Act
+        Assert.Throws<InvalidOperationException>(() => dummy.GiveExperience());
+    }
+}
